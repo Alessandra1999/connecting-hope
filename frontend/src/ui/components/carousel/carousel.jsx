f25b52a574a2bdb9a1c9360d6cabbe0ec30109ca
akemicomette <akemicomette@gmail.com>
2024-11-01 20:23:09 -0300

    feat: initial page showCampaign

---
+++ frontend/src/ui/components/carousel/carousel.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+import { Carousel } from '@material-tailwind/react';
+
+export function CarouselDefault({ images }) {
+  return (
+    <div className="mx-16 my-4">
+      <Carousel
+        autoplay={true}
+        loop={true}
+        transition={{ duration: 1 }}
+        navigation={false}
+        className="rounded-xl overflow-hidden"
+      >
+        {images.map(({ img, alt }, idx) => (
+          <img
+            key={`${alt}_${idx}`}
+            src={img}
+            alt={alt}
+            className="h-80 w-full object-cover object-center"
+          />
+        ))}
+      </Carousel>
+    </div>
+  );
+}
---
+++ frontend/src/ui/screens/login/index.jsx
@@ -0,0 +1,5 @@
+export function LoginPage() {
+  return (
+    <div className="w-full h-screen bg-gradient-to-b from-primary-light-250 to-primary-light-400"></div>
+  );
+}
