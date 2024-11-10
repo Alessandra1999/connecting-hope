ab292f33022ee8c0870a11085b856f87096185bd
devisrael <devisrael560@gmail.com>
2024-11-10 17:46:56 -0300

    add: theme and keyframes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,7 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
+ theme: {  darkMode: ['selector', '[data-mode="dark"]'],
     container: {
       center: true,
     },
@@ -13,6 +13,20 @@ module.exports = withMT({
       sans: ["Montserrat", "sans-serif"],
     },
     extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'scale(0.95)' },
+          '100%': { opacity: '1', transform: 'scale(1)' },
+        },
+        fadeOut: {
+          '0%': { opacity: '1', transform: 'scale(1)' },
+          '100%': { opacity: '0', transform: 'scale(0.95)' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-out forwards',
+        fadeOut: 'fadeOut 0.5s ease-out forwards',
+      },
       colors: {
         "primary-light": {
           50: "#FFF",
