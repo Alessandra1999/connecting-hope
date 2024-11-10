d7037a49a7ac7a22b400f3071f89130c53298139
devisrael <devisrael560@gmail.com>
2024-11-10 17:48:33 -0300

    fix: removing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
 import { BrowserRouter } from 'react-router-dom';
-import { Navbar } from './ui/components/navbar';
 import { Router } from './router';
 import Slider from './ui/components/slider/slider';
 import { Home } from './ui/screens/home';
 import { HomeNotLogged } from './ui/screens/HomeNotLogged';
 import { InitialScreen } from './ui/screens/register&login';
-import LoginForm from './ui/components/registers-forms/LoginForm';
-import UserForm from './ui/components/registers-forms/UserForm';
 
 function App() {
   return (
