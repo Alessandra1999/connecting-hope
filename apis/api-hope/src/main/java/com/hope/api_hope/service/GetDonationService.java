4b5716db201d90b465004909db52d6ad218086b8
Alessandra Bombardi <ale.brbr@gmail.com>
2024-11-13 20:13:34 -0300

    start of stripe api implementation

--- apis/api-hope/src/main/java/com/hope/api_hope/service/GetDonationService.java
+++ apis/api-hope/src/main/java/com/hope/api_hope/service/GetDonationService.java
@@ -3,7 +3,6 @@ package com.hope.api_hope.service;
 import com.hope.api_hope.dto.response.DonationResponse;
 import com.hope.api_hope.mapper.DonationMapper;
 import com.hope.api_hope.repository.DonationRepository;
-import com.hope.api_hope.repository.NonProfitsCampaignRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
