88510787811ad7ad85df26966ca73ab02736e20d
Alessandra <ale.brbr@gmail.com>
2024-11-16 10:02:25 -0300

    Revert "Feature/create home page"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Home } from './ui/screens/home';
-import { HomeNotLogged } from './ui/screens/HomeNotLogged';
-import { InitialScreen } from './ui/screens/register&login';
+
 
 function App() {
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<InitialScreen />} />
-        <Route path="/homeNotLogged" element={<HomeNotLogged />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+    <>
+      <h1>Connecting Hope</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
--- frontend/src/ui/screens/profile/profile.jsx
+++
@@ -1,7 +0,0 @@
-export function Profile() {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  );
-}
